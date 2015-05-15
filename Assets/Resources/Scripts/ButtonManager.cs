e0f277349eb3ade7ed4e6993ef189c60a864d707
Sacha Best <sachabest@outlook.com>
2015-05-15 14:27:17 -0700

    Updaing comments

---
+++ Assets/Resources/Scripts/ButtonManager.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ButtonManager : MonoBehaviour {
+
+	public Text lifeName, lifeScore, industryName, industryScore, rollDisplay;
+	public static ButtonManager instance;
+
+	// Use this for initialization
+	void Start () {
+		instance = this;
+		lifeScore.text = "0" + "/" + GameManager.instance.winningScore;
+		industryScore.text = "0" + "/" + GameManager.instance.winningScore;
+		rollDisplay.text = "0";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void Undo()
+	{
+		GameManager.instance.Undo();
+	}
+	public void Tier1()
+	{
+		GameManager.instance.SetTier(1);
+	}
+	public void Tier2()
+	{
+		GameManager.instance.SetTier(2);
+	}
+	public void Tier3()
+	{
+		GameManager.instance.SetTier(3);
+	}
+	public void Turn() 
+	{
+		GameManager.instance.NextTurn();
+	}
+}
